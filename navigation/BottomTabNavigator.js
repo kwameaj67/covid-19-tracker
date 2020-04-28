347546ac8d685a40febc470a00415285aae7d298
unknown <kwameagyenimboateng64@gmail.com>
2020-04-28 15:24:32 +0000

    change name of bottom nav

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -20,16 +20,16 @@ export default function BottomTabNavigator({ navigation, route }) {
         name="Home"
         component={HomeScreen}
         options={{
-          title: 'Get Started',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
+          title: 'Home',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="home" />,
         }}
       />
       <BottomTab.Screen
         name="Links"
         component={LinksScreen}
         options={{
-          title: 'Resources',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
+          title: 'Wallet',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="wallet" />,
         }}
       />
     </BottomTab.Navigator>
@@ -41,8 +41,8 @@ function getHeaderTitle(route) {
 
   switch (routeName) {
     case 'Home':
-      return 'How to get started';
+      return 'COVID-19 Worldwide';
     case 'Links':
-      return 'Links to learn more';
+      return 'Settings';
   }
 }
