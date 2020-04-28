927b54c47cef5628868849999c4ebb480df12d2c
unknown <kwameagyenimboateng64@gmail.com>
2020-04-28 02:53:21 +0000

    changed icon

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -1,13 +1,13 @@
-import { Ionicons } from '@expo/vector-icons';
+import {SimpleLineIcons } from '@expo/vector-icons';
 import * as React from 'react';
 
 import Colors from '../constants/Colors';
 
 export default function TabBarIcon(props) {
   return (
-    <Ionicons
+    <SimpleLineIcons
       name={props.name}
-      size={30}
+      size={24}
       style={{ marginBottom: -3 }}
       color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
     />
