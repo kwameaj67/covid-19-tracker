0d52f38db44d9765fdef33f19abd55ddfb223445
unknown <kwameagyenimboateng64@gmail.com>
2020-04-28 00:07:24 +0000

    added apollo client

---
+++ Graphql/Client.js
@@ -0,0 +1,17 @@
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createHttpLink } from 'apollo-link-http'
+import { api } from '../config'
+
+const httplink = createHttpLink({
+    uri:api
+})
+
+const client = new ApolloClient({
+    link:httplink,
+    cache:InMemoryCache
+})
+
+module.exports = {
+    client
+}
