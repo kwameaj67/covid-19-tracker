8264a16ec6c79062b83178242c46ede34d9d6efb
unknown <kwameagyenimboateng64@gmail.com>
2020-05-31 21:44:50 +0000

    add country selection screen

---
+++ screens/Auth/Country.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {StyleSheet,View,Text,SafeAreaView} from 'react-native'
+
+function Country() {
+    return (
+        <SafeAreaView style={{flex:1}}>
+            <View style={styles.container}>
+                <Text> Faq</Text>
+            </View>
+        </SafeAreaView>
+        );
+} 
+
+export default Country;
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+})
