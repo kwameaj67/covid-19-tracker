4c031bf757256d29a3f64505ce47e0bf0a12a072
unknown <kwameagyenimboateng64@gmail.com>
2020-05-31 21:45:13 +0000

    added props, modified icons

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -1,15 +1,44 @@
-import {SimpleLineIcons } from '@expo/vector-icons';
+import {SimpleLineIcons,MaterialCommunityIcons,MaterialIcons,AntDesign,Fontisto } from '@expo/vector-icons';
 import * as React from 'react';
 
 import Colors from '../constants/Colors';
 
 export default function TabBarIcon(props) {
-  return (
-    <SimpleLineIcons
-      name={props.name}
-      size={24}
-      style={{ marginBottom: -3 }}
-      color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
-    />
-  );
+  if(this.props.icon ==="home"){
+    return (
+      <MaterialCommunityIcons
+        name={props.name}
+        size={24}
+        style={{ marginBottom: -3 }}
+        color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+      />
+    );
+  }else  if(this.props.icon ==="report"){
+    return (
+      <MaterialIcons
+        name={props.name}
+        size={24}
+        style={{ marginBottom: -3 }}
+        color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+      />
+    );
+  }else  if(this.props.icon ==="health"){
+    return (
+      <Fontisto
+        name={props.name}
+        size={24}
+        style={{ marginBottom: -3 }}
+        color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+      />
+    );
+  }else  if(this.props.icon ==="faq"){
+    return (
+      <AntDesign
+        name={props.name}
+        size={24}
+        style={{ marginBottom: -3 }}
+        color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+      />
+    );
+  }
 }
