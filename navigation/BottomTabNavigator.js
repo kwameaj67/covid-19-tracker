7f7bd5f54aff2f5182b754095d993b855a0a4234
unknown <kwameagyenimboateng64@gmail.com>
2020-05-31 21:45:31 +0000

    modified bottomTabNav

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -2,8 +2,11 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import * as React from 'react';
 
 import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
+import Home from '../screens/Home/Home';
+import Report from '../screens/Report/Report';
+import Health from '../screens/Health/Health';
+import Faq from '../screens/FAQ/Faq';
+
 
 const BottomTab = createBottomTabNavigator();
 const INITIAL_ROUTE_NAME = 'Home';
@@ -18,18 +21,34 @@ export default function BottomTabNavigator({ navigation, route }) {
     <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
       <BottomTab.Screen
         name="Home"
-        component={HomeScreen}
+        component={Home}
         options={{
           title: 'Home',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="home" />,
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="home-variant" icon="home"/>,
+        }}
+      />
+      <BottomTab.Screen
+        name="Report"
+        component={Report}
+        options={{
+          title: 'Report',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="alarm" icon="report" />,
         }}
       />
       <BottomTab.Screen
         name="Links"
-        component={LinksScreen}
+        component={Health}
         options={{
-          title: 'Wallet',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="wallet" />,
+          title: 'Health',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="hearthbeat-alt" icon="health" />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Links"
+        component={Faq}
+        options={{
+          title: 'FAQs',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="profile" icon="faq" />,
         }}
       />
     </BottomTab.Navigator>
