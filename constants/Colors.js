226c85adca8aaa681b199f99d9829d6ef487ce51
unknown <kwameagyenimboateng64@gmail.com>
2020-05-31 21:44:59 +0000

    added new colors

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,9 +1,19 @@
 const tintColor = '#2f95dc';
+const grey = "#A7A7A7";
+const text = "#5F5D5D";
+const background = "#F8F8F8";
+const border = '#e5e5e5';
+const black = '#686874'
 
 export default {
   tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
+  grey,
+  text,
+  border,
+  black,
+  background,
+  tabIconDefault: grey,
+  tabIconSelected: black,
   tabBar: '#fefefe',
   errorBackground: 'red',
   errorText: '#fff',
