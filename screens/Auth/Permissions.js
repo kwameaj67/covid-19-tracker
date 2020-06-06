e627e772e4cde7815d4f57947cf15b91afc21456
unknown <kwameagyenimboateng64@gmail.com>
2020-06-06 14:01:44 +0000

    added permissions screen

---
+++ screens/Auth/Permissions.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {StyleSheet,View,Text,SafeAreaView} from 'react-native'
+
+function Permission() {
+    return (
+        <SafeAreaView style={{flex:1}}>
+            <View style={styles.container}>
+                <Text> Faq</Text>
+            </View>
+        </SafeAreaView>
+        );
+} 
+
+export default Permission;
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+})
