4549ebe47bc95b7991b2d91e015d7eb30a466ad8
unknown <kwameagyenimboateng64@gmail.com>
2020-06-07 23:27:15 +0000

    modified permissions

--- screens/Auth/Permissions.js
+++ screens/Auth/Permissions.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import {StyleSheet,View,Text,SafeAreaView} from 'react-native'
+import Button from '../../components/Button'
+import Colors from '../../constants/Colors'
+import Layout from '../../constants/Layout'
+import Fonts from '../../constants/Fonts'
 
 function Permission() {
     return (
         <SafeAreaView style={{flex:1}}>
             <View style={styles.container}>
-                <Text> Faq</Text>
+                <Text> Permission</Text>
             </View>
         </SafeAreaView>
         );
