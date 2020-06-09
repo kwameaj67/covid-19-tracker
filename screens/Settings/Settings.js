ef5118d066adde764c89578e5b33abd0e49902a6
unknown <kwameagyenimboateng64@gmail.com>
2020-06-09 16:33:00 +0000

    modified settings screen

--- screens/Settings/Settings.js
+++ screens/Settings/Settings.js
@@ -1,14 +1,55 @@
 import React from 'react'
-import {StyleSheet,View,Text,SafeAreaView} from 'react-native'
+import {StyleSheet,View,Text,SafeAreaView,TouchableOpacity,TouchableNativeFeedback,Platform} from 'react-native'
+import ActionSheet from 'react-native-actions-sheet'
 import Colors from '../../constants/Colors'
 import Layout from '../../constants/Layout'
-import ActionSheet from 'react-native-actions-sheet'
 import Fonts from '../../constants/Fonts'
+import Header from '../../components/Header'
+import { AntDesign,Ionicons,FontAwesome }  from '@expo/vector-icons'
 function Settings() {
+    const LeftView = ()=> {
+        return (
+          <View>
+            {Platform.OS === 'ios' ?
+            <TouchableOpacity activeOpacity={0.8} onPress={()=>{}}>
+            <View style={styles.backBtn}>
+            <FontAwesome name="user-circle-o" size={24} color="black" />
+            </View>
+          </TouchableOpacity>
+          :
+          <TouchableNativeFeedback onPress={() =>{}} background={TouchableNativeFeedback.SelectableBackgroundBorderless()} useForeground={true} >
+            <View style={styles.backBtn}>
+            <FontAwesome name="user-circle-o" size={24} color="black" />
+            </View>
+          </TouchableNativeFeedback>
+            }
+          </View>
+        );
+  }
+  const RightView = ()=> {
     return (
-        <SafeAreaView style={{flex:1}}>
+      <View>
+        {Platform.OS === 'ios' ?
+        <TouchableOpacity activeOpacity={0.8} onPress={()=>{}}>
+        <View style={styles.backBtn}>
+        <Ionicons name="ios-notifications-outline" size={24} color="black" />
+        </View>
+      </TouchableOpacity>
+      :
+      <TouchableNativeFeedback onPress={() =>{}} background={TouchableNativeFeedback.SelectableBackgroundBorderless()} useForeground={true} >
+        <View style={styles.backBtn}>
+        <Ionicons name="ios-notifications-outline" size={24} color="black" />
+        </View>
+      </TouchableNativeFeedback>
+        }
+      </View>
+    );
+}
+    return (
+        <SafeAreaView style={{flex:1,backgroundColor:'white'}}>
+          <Header leftAction={LeftView()} rightAction={RightView()} title="Vitals" type="App"/>
             <View style={styles.container}>
-                <Text> Settings</Text>
+                <Text>Settings</Text>
             </View>
         </SafeAreaView>
         );
