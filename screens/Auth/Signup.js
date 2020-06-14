039c1e09b3257ff7fa097fe3a5ab4e4339cbe391
unknown <kwameagyenimboateng64@gmail.com>
2020-06-14 12:08:27 +0000

    changes to signup

--- screens/Auth/Signup.js
+++ screens/Auth/Signup.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import {StyleSheet,View,Text,SafeAreaView} from 'react-native'
-
+import Button from '../../components/Button'
+import Input from '../../components/TextInput'
+import Colors from '../../constants/Colors'
+import Fonts from '../../constants/Fonts'
+import Layout from '../../constants/Layout'
 function Signup() {
     return (
         <SafeAreaView style={{flex:1}}>
             <View style={styles.container}>
-                <Text> Faq</Text>
+                <Text> Signup</Text>
             </View>
         </SafeAreaView>
         );
