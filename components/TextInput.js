7b01c35898e8b9ae0c674c5cacc086f04eef6913
unknown <kwameagyenimboateng64@gmail.com>
2020-06-22 19:45:59 +0000

    modified text input

--- components/TextInput.js
+++ components/TextInput.js
@@ -1,120 +1,50 @@
 import React from 'react'
 import { View,StyleSheet,TextInput } from 'react-native'
 import Colors from '../constants/Colors'
+import Layout from '../constants/Layout'
 
 export default function Input(props){
-   if(props.input === "name"){
+   if(props.input === "general"){
     return(
-        <View style={{paddingHorizontal: 10,}}>
-           <View style={styles.textContainer}>
+        <View>
             <TextInput
-                    style={styles.input}
                     placeholder={props.placeholder}
-                    textContentType="name"
-                    fontSize={16}
-                    placeholderTextColor={Colors.grey}
-                    autoFocus={true}
-                    maxLength={180}
-                    multiline={true}
-                    onChange={props.onChange}
-                />
-           </View>
-        </View>
-    )
-   }else if(props.input === "email"){
-    return(
-        <View style={{paddingHorizontal: 10,}}>
-           <View style={styles.textContainer}>
-            <TextInput
-                    // ref = { input => { this.textInput = input}}
-                    style={styles.input}
-                    placeholder={props.placeholder}
-                    textContentType="username"
-                    keyboardType="email-address"
-                    fontSize={16}
-                    placeholderTextColor={Colors.grey}
-                    autoFocus={false}
-                    maxLength={180}
-                    multiline={true}
-                    onChange={props.onChange}
-                />
-           </View>
-        </View>
-    )
-   }else if(props.input === "password"){
-    return(
-        <View style={{paddingHorizontal: 10,}}>
-           <View style={styles.textContainer}>
-            <TextInput
-                    // ref = { input => { this.textInput = input}}
-                    style={styles.input}
-                    placeholder={props.placeholder}
-                    textContentType="password"
                     keyboardType="default"
                     fontSize={16}
-                    placeholderTextColor={Colors.grey}
-                    autoFocus={false}
-                    maxLength={180}
-                    multiline={true}
-                    onChange={props.onChange}
+                    onChangeText={props.onChange}
+                    style={styles.input}
                 />
-           </View>
         </View>
     )
    }else if(props.input === "number"){
-    return(
-        <View style={{paddingHorizontal: 10,}}>
-           <View style={styles.textContainer}>
-            <TextInput
-                    style={styles.input}
-                    placeholder={props.placeholder}
-                    textContentType="telephoneNumber"
-                    keyboardType="number-pad"
-                    fontSize={16}
-                    placeholderTextColor={Colors.grey}
-                    autoFocus={false}
-                    maxLength={10}
-                    multiline={true}
-                    onChange={props.onChange}
-                />
-           </View>
-        </View>
-    )
-   }else if(props.input === "code"){
-    return(
-        <View style={{paddingHorizontal: 10,}}>
-           <View style={styles.textContainer}>
-            <TextInput
-                    // ref = { input => { this.textInput = input}}
-                    style={styles.input}
-                    placeholder={props.placeholder}
-                    textContentType="telephoneNumber"
-                    keyboardType="number-pad"
-                    fontSize={16}
-                    placeholderTextColor={Colors.grey}
-                    autoFocus={false}
-                    maxLength={4}
-                    multiline={true}
-                    onChange={props.onChange}
-                    value={props.value}
-                />
-           </View>
-        </View>
-    )
-   }
+      return(
+          <View>
+              <TextInput
+                      placeholder={props.placeholder}
+                      keyboardType="number-pad"
+                      fontSize={16}
+                      onChangeText={props.onChange}
+                      style={styles.input}
+                  />
+          </View>
+      )
+     }
+  
 
 }
 const styles = StyleSheet.create({
-    input: {
-		letterSpacing: 0,
-		fontSize: 18,
-		color: Colors.placeholder,
-		fontFamily: "Medium",
-      },
       textContainer:{
         borderColor:Colors.border,
         borderWidth:1,
         borderRadius:5,
         padding:7,
-      }
+      },
+       input: {
+        letterSpacing: 0,
+        fontSize: 14,
+        color: Colors.grey,
+        fontFamily: "Medium",
+        marginVertical: 10,
+        borderColor: Colors.border,
+          },
 })
