e14116dd0d7da2bdb28b87fe6088b498d965369b
unknown <kwameagyenimboateng64@gmail.com>
2020-06-22 19:47:04 +0000

    editing changes

--- constants/body.js
+++ constants/body.js
@@ -99,48 +99,40 @@ export const News = [
     {
       id: 9,
       title: 'Options regarding your information',
-      info:
-        'Citizens are advised to provide accurate information on this application to support the government and health sevices in managing and accurately containing the spread of coronavirus',
+      info:'Citizens are advised to provide accurate information on this application to support the government and health sevices in managing and accurately containing the spread of coronavirus',
     },
     {
       id: 10,
       title: 'Contact us',
-      info:
-        'Citizens are advised to provide accurate information on this application to support the government and health sevices in managing and accurately containing the spread of coronavirus',
+      info:'Citizens are advised to provide accurate information on this application to support the government and health sevices in managing and accurately containing the spread of coronavirus',
     },
   ];
   
   export const FAQsContent = [
     {
       title: 'What is Coronavirus?',
-      content:
-        'Coronavirus is a large group of viruses that are common among animals. In rare cases, they are what scientists call zoonotic, meaning they can be transmitted from animals to humans, according to the US Centers for Disease Control and Prevention. It is a dangerous diseases with incubation period between 4-6 days.',
+      content:'Coronavirus is a large group of viruses that are common among animals. In rare cases, they are what scientists call zoonotic, meaning they can be transmitted from animals to humans, according to the US Centers for Disease Control and Prevention. It is a dangerous diseases with incubation period between 4-6 days.',
     },
     {
       title: 'What are the symptoms of COVID-19?',
-      content:
-        'The viruses can make people sick. Coronavirus symptoms include;fever,a runny nose,cough,sore throat and possibly a headache',
+      content:'The viruses can make people sick. Coronavirus symptoms include;fever,a runny nose,cough,sore throat and possibly a headache',
     },
     {
       title: 'How does COVID-19 spread?',
-      content:
-        'When it comes to human-to-human transmission of the viruses, often it happens when someone comes into contact with the infected persons secretions. The exposure factors are; a cough, sneeze or handshake',
+      content:'When it comes to human-to-human transmission of the viruses, often it happens when someone comes into contact with the infected persons secretions. The exposure factors are; a cough, sneeze or handshake',
     },
     {
       title:
         'What can I do to protect myself & prevent the spread of the disease?',
-      content:
-        'Wash your hands with soap and water before touching anything including your eyes, nose and mouth. Cover your mouth and nose when you cough or sneeze. Disinfect the objects and surfaces you touch. If you are sick, stay home and avoid crowds and contact with others.',
+      content:'Wash your hands with soap and water before touching anything including your eyes, nose and mouth. Cover your mouth and nose when you cough or sneeze. Disinfect the objects and surfaces you touch. If you are sick, stay home and avoid crowds and contact with others.',
     },
     {
       title: 'How likely am I to catch COVID-19?',
-      content:
-        'The virus can also be transmitted by touching something an infected person has touched and then touching your mouth, nose or eyes.',
+      content:'The virus can also be transmitted by touching something an infected person has touched and then touching your mouth, nose or eyes.',
     },
     {
       title: 'What should you do if you think you have COVID-19',
-      content:
-        'Call your doctor for advice if you think you’ve been exposed to coronavirus and develop a fever or cough. If you don’t have a primary care physician, call your local board of public health or local hospital. Due to the limited availability of test kits, your doctor will only test you if necessary. If he decides not to test you, he will likely advise you to self-isolate at home until your symptoms are gone.',
+      content:'Call your doctor for advice if you think you’ve been exposed to coronavirus and develop a fever or cough. If you don’t have a primary care physician, call your local board of public health or local hospital. Due to the limited availability of test kits, your doctor will only test you if necessary. If he decides not to test you, he will likely advise you to self-isolate at home until your symptoms are gone.',
     },
     {
       title: 'Who is at risk for developing severe illness?',
